0fcb26a4eb605b7ffa837ec6f848010eec3270cd
Elmalik07 <malik.tiemogo@gmail.com>
2022-06-13 18:16:32 +0000

    pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('It work!'));
+app.get('/', (req, res) => res.send('Test du pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
